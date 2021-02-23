ea3b4f6ee84a75cbdd49a52d45b1e8098bb87a88
joseph.li <joseph.li@yingzt.com>
2021-02-23 17:18:53 +0800

    add zmq3 demo

--- demo/client/client_demo.go
+++ demo/client/client_demo.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/pebbe/zmq3"
+	"log"
+)
 
+func main() {
+	context, _ := zmq3.NewContext()
+	socket, _ := context.NewSocket(zmq3.REQ)
+
+	err := socket.Connect("tcp://127.0.0.1:5000")
+	if err != nil {
+		log.Fatal("socket connect occur err:", err)
+	}
+
+	_, err = socket.Send("hello world", 0)
+	if err != nil {
+		log.Fatal("socket send occur err:", err)
+	}
 }
